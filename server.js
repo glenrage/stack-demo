33af7347ea2fb60f68674a8b300313fdd7fcad44
Glen Pham <phamgalang@gmail.com>
2020-10-25 16:18:34 -0700

    edit server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.get('/api/products/id/data', (req, res) => {
   try {
