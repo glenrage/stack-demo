d52980f3b7bd4a8492c6c4c0d688e270fa80b718
Glen Pham <phamgalang@gmail.com>
2020-10-25 16:40:31 -0700

    update server

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require("path")
+const data = require('./db/data.json')
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
+
+const buildPath = path.join(__dirname, 'client', 'build');
+app.use(express.static(buildPath));
+console.log(buildPath)
 
 app.get('/api/products/id/data', (req, res) => {
   try {
-    const data = require('./db/data.json')
     res.header("Content-Type",'application/json');
     res.send(JSON.stringify(data));
   } catch(err)  {
