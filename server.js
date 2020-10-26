776a26eaa66fa477b5455290c051c0d0a724b609
Glen Pham <phamgalang@gmail.com>
2020-10-25 17:11:09 -0700

    add db

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require("path")
-const productData = require('./db/data')
 const data = require('./db/data.json')
 
 // app.use(express.static(path.join(__dirname, 'client/build')));
